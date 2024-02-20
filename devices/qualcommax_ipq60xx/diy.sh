ff4f44cc673e3234b88323ebb088a45dcd6bb6da
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-02-20 17:02:59 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -4,8 +4,13 @@ shopt -s extglob
 
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-git clone https://github.com/JiaY-shi/nss-packages.git package/nss-packages
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/generic/hack-6.1
 
-rm -rf feeds/kiddin9/{fibocom_QMI_WWAN,quectel_Gobinet,shortcut-fe}
+git_clone_path master https://github.com/coolsnowwolf/lede target/linux/qualcommax
 
-rm -rf devices/common/patches/rootfstargz.patch devices/common/patches/kernel_version.patch
+rm -rf package/feeds/kiddin9/quectel_Gobinet devices/common/patches/kernel_version.patch devices/common/patches/rootfstargz.patch
+
+curl -sfL https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch -o target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch
+
+
+
